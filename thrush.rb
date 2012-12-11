7c368cb5a8aab526f6cf329300b80b7ce7609922
coreyhaines <coreyhaines@gmail.com>
2012-12-10 15:38:12 -1000

    reusable test method

--- thrush.rb
+++ thrush.rb
@@ -1,4 +1,4 @@
-thrush = ->(proc1) do
+Thrush = ->(proc1) do
   ->(proc2) {
     ->(val) {
       proc2.to_proc.(proc1.to_proc.(val))
@@ -6,19 +6,26 @@ thrush = ->(proc1) do
   }
 end
 
+def test msg = "FAILURE", &conditions
+  yield or raise msg
+  STDOUT << "."
+end
 
 # Thrush
 
-thrush.call(->(x){x*2}).call(->(x){ x + 1}).(3) == 7 or raise "thrush should call in reverse order"
-STDOUT << "."
+test "thrush should call in reverse order" do
+  Thrush.call(->(x){x*2}).call(->(x){ x + 1}).(3) == 7
+end
+
 
 
 # can store the combination and apply later
-times_2 = ->(x) { x * 2 }
-plus_2 = ->(x) { x + 2 }
-times_2_then_plus_2 = thrush.call(times_2).call(plus_2)
+test "thrush should allow for storing combination for later application" do
+  times_2 = ->(x) { x * 2 }
+  plus_2 = ->(x) { x + 2 }
+  times_2_then_plus_2 = Thrush.call(times_2).call(plus_2)
 
-times_2_then_plus_2.(5) == 12 or raise "thrush should allow for storing combination for later application"
-STDOUT << "."
+  times_2_then_plus_2.(5) == 12
+end
 
 STDOUT << "\n"
